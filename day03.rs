4712a5c251726ac749f5cc523db696df9f8b95a8
Frans Saukko <frans@equilibrium.co>
2019-12-03 15:57:28 +0200

    aoc2019d03: reimplement logic

--- day03.rs
+++ day03.rs
@@ -4,70 +4,25 @@ use std::fmt::Debug;
 
 #[derive(Clone,Debug)]
 pub enum Direction {
-	Up(usize),
-	Down(usize),
-	Left(usize),
-	Right(usize),
+	Up(i32),
+	Down(i32),
+	Left(i32),
+	Right(i32),
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone,Copy,Debug)]
 pub struct Wire {
-	id: usize,
+	id: u32,
+	xy: [i32;2],
+	dim: [i32;2],
 }
 
 impl Wire {
-	pub fn new (id: usize) -> Wire {
+	pub fn new (id: u32, xy: [i32;2], dim: [i32;2]) -> Wire {
 		Wire {
 			id,
-		}
-	}
-}
-
-pub struct WireGrid {
-	grid: Vec<Vec<Vec<Wire>>>,
-}
-
-impl WireGrid {
-	pub fn new (wh: [usize;2]) -> WireGrid {
-		WireGrid {
-			grid: vec![vec![Vec::new();wh[0]];wh[1]],
-		}
-	}
-
-	fn at (&self, xy: [usize;2]) -> &Vec<Wire> {
-		&self.grid[xy[1]][xy[0]]
-	}
-
-	fn at_mut (&mut self, xy: [usize;2]) -> &mut Vec<Wire> {
-		&mut self.grid[xy[1]][xy[0]]
-	}
-
-	fn center (&mut self) -> [usize;2] {
-		[self.grid[0].len()/2,self.grid.len()/2]
-	}
-
-	pub fn add (&mut self, ds: &[Direction], w: Wire) {
-		let mut xy = self.center();
-		self.at_mut(xy).push(w);
-		for d in ds {
-			match d {
-				&Direction::Up(x)		=>	for _ in 0..x {
-												xy[0] -= 1;
-												self.at_mut(xy).push(w);
-											},
-				&Direction::Down(x)		=>	for _ in 0..x {
-												xy[0] += 1;
-												self.at_mut(xy).push(w);
-											},
-				&Direction::Left(x)		=>	for _ in 0..x {
-												xy[1] -= 1;
-												self.at_mut(xy).push(w);
-											},
-				&Direction::Right(x)	=>	for _ in 0..x {
-												xy[1] += 1;
-												self.at_mut(xy).push(w);
-											},
-			}
+			xy,
+			dim,
 		}
 	}
 }
@@ -78,13 +33,13 @@ fn parse (s: &str) -> Vec<Vec<Direction>> {
 	let mut ns = None;
 	let mut dir = None;
 	for c in s.chars() {
-		match c as usize {
+		match c as u32 {
 			10			=>	{
 								v.push(match dir {
-									Some('U')	=>	Direction::Up(ns.unwrap()),
-									Some('D')	=>	Direction::Down(ns.unwrap()),
-									Some('L')	=>	Direction::Left(ns.unwrap()),
-									Some('R')	=>	Direction::Right(ns.unwrap()),
+									Some('U')	=>	Direction::Up(ns.unwrap() as i32),
+									Some('D')	=>	Direction::Down(ns.unwrap() as i32),
+									Some('L')	=>	Direction::Left(ns.unwrap() as i32),
+									Some('R')	=>	Direction::Right(ns.unwrap() as i32),
 									_			=>	unreachable!(),
 								});
 								dir = None;
@@ -94,10 +49,10 @@ fn parse (s: &str) -> Vec<Vec<Direction>> {
 							},
 			44			=>	{
 								v.push(match dir {
-									Some('U')	=>	Direction::Up(ns.unwrap()),
-									Some('D')	=>	Direction::Down(ns.unwrap()),
-									Some('L')	=>	Direction::Left(ns.unwrap()),
-									Some('R')	=>	Direction::Right(ns.unwrap()),
+									Some('U')	=>	Direction::Up(ns.unwrap() as i32),
+									Some('D')	=>	Direction::Down(ns.unwrap() as i32),
+									Some('L')	=>	Direction::Left(ns.unwrap() as i32),
+									Some('R')	=>	Direction::Right(ns.unwrap() as i32),
 									_			=>	unreachable!(),
 								});
 								dir = None;
@@ -119,10 +74,48 @@ fn main () -> std::io::Result<()> {
 	let mut file = File::open("input03")?;
 	let mut data = String::new();
 	file.read_to_string(&mut data)?;
-	let mut wg = WireGrid::new([4096,4096]);
+	let mut superwires = Vec::new();
 	for (i,ds) in parse(&data).iter().enumerate() {
-		let w = Wire::new(i);
-		wg.add(&ds,w);
+		let mut wires = Vec::new();
+		let mut xy = [0,0];
+		for d in ds {
+			let dim = match d {
+				&Direction::Up(y)	=>	[0,y],
+				&Direction::Down(y)	=>	[0,-y],
+				&Direction::Left(x)	=>	[-x,0],
+				&Direction::Right(x)	=>	[x,0],
+			};
+			let wdim = [dim[0]/2,dim[1]/2];
+			wires.push(Wire::new(i as u32,[xy[0] + wdim[0],xy[1] + wdim[1]],wdim));
+			xy[0] += dim[0];
+			xy[1] += dim[1];
+		}
+		superwires.push(wires);
 	}
+	let mut nearest = std::i32::MAX;
+	let (wsa,wsb) = superwires.split_first().unwrap();
+	for a in wsa {
+		for b in &wsb[0] {
+			if (a.xy[0] - b.xy[0]).abs() <= a.dim[0] + b.dim[0]
+			&& (a.xy[1] - b.xy[1]).abs() <= a.dim[1] + b.dim[1] {
+				let cx = [if a.dim[0] == 0 {
+					a.xy[0]
+				}
+				else {
+					b.xy[0]
+				},if a.dim[1] == 0 {
+					a.xy[1]
+				}
+				else {
+					b.xy[1]
+				}];
+				let md = cx[0].abs() + cx[1].abs();
+				if md < nearest {
+					nearest = md;
+				}
+			}
+		}
+	}
+	println!("SHORTEST MANHATTAN DISTANCE:\t{}",nearest);
 	Ok(())
 }
