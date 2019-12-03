bb257d10c5c27296d6dd9e067b90d279a6757565
Frans Saukko <frans@equilibrium.co>
2019-12-03 13:58:41 +0200

    Prototype grid solution

--- day03.rs
+++ day03.rs
@@ -1,36 +1,63 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::fmt::{Debug,Formatter,Result};
+use std::fmt::Debug;
 
-enum Direction {
+#[derive(Clone,Debug)]
+pub enum Direction {
 	Up(usize),
 	Down(usize),
 	Left(usize),
 	Right(usize),
 }
 
-impl Debug for Direction {
-	fn fmt (&self, f: &mut Formatter<'_>) -> Result {
-		let mut s = String::new();
-		match self {
-			Direction::Up(x)	=>	{
-										s.push_str("up:");
-										s.push_str(&x.to_string());
-									},
-			Direction::Down(x)	=>	{
-										s.push_str("down:");
-										s.push_str(&x.to_string());
-									},
-			Direction::Left(x)	=>	{
-										s.push_str("left:");
-										s.push_str(&x.to_string());
-									},
-			Direction::Right(x)	=>	{
-										s.push_str("right:");
-										s.push_str(&x.to_string());
-									},
-		};
-		write!(f,"{}",s)
+#[derive(Clone,Copy)]
+pub struct Wire {
+	id: usize,
+}
+
+impl Wire {
+	pub fn new (id: usize) -> Wire {
+		Wire {
+			id,
+		}
+	}
+}
+
+pub struct WireGrid {
+	grid: Vec<Vec<Vec<Wire>>>,
+}
+
+impl WireGrid {
+	pub fn new (wh: [usize;2]) -> WireGrid {
+		WireGrid {
+			grid: vec![vec![Vec::new();wh[0]];wh[1]],
+		}
+	}
+
+	fn at (&self, xy: [usize;2]) -> &Vec<Wire> {
+		&self.grid[xy[1]][xy[0]]
+	}
+
+	fn at_mut (&mut self, xy: [usize;2]) -> &mut Vec<Wire> {
+		&mut self.grid[xy[1]][xy[0]]
+	}
+
+	fn center (&mut self) -> &mut Vec<Wire> {
+		let (y,x) = (self.grid.len()/2,self.grid[0].len()/2);
+		&mut self.grid[y][x]
+	}
+
+	pub fn add (&mut self, ds: &[Direction], w: Wire) {
+		let xy = self.center();
+		self.center().push(w);
+		for d in ds {
+			match d {
+				Direction::Up(x)	=>	println!("up({}):{}",w.id,x),
+				Direction::Down(x)	=>	println!("down({}):{}",w.id,x),
+				Direction::Left(x)	=>	println!("left({}):{}",w.id,x),
+				Direction::Right(x)	=>	println!("right({}):{}",w.id,x),
+			}
+		}
 	}
 }
 
@@ -41,37 +68,37 @@ fn parse (s: &str) -> Vec<Vec<Direction>> {
 	let mut dir = None;
 	for c in s.chars() {
 		match c as usize {
-			10							=>	{
-												v.push(match dir {
-													Some('U')	=>	Direction::Up(ns.unwrap()),
-													Some('D')	=>	Direction::Down(ns.unwrap()),
-													Some('L')	=>	Direction::Left(ns.unwrap()),
-													Some('R')	=>	Direction::Right(ns.unwrap()),
-													_			=>	unreachable!(),
-												});
-												dir = None;
-												ns = None;
-												wires.push(v);
-												v = Vec::new();
-											},
-			44							=>	{
-												v.push(match dir {
-													Some('U')	=>	Direction::Up(ns.unwrap()),
-													Some('D')	=>	Direction::Down(ns.unwrap()),
-													Some('L')	=>	Direction::Left(ns.unwrap()),
-													Some('R')	=>	Direction::Right(ns.unwrap()),
-													_			=>	unreachable!(),
-												});
-												dir = None;
-												ns = None;
-										},
-			n @ 48..=57					=>	if let Some(x) = ns {
-												ns = Some(10*x + n - 48)
-											}
-											else {
-												ns = Some(n - 48)
-											},
-			_ 							=>	dir = Some(c),
+			10			=>	{
+								v.push(match dir {
+									Some('U')	=>	Direction::Up(ns.unwrap()),
+									Some('D')	=>	Direction::Down(ns.unwrap()),
+									Some('L')	=>	Direction::Left(ns.unwrap()),
+									Some('R')	=>	Direction::Right(ns.unwrap()),
+									_			=>	unreachable!(),
+								});
+								dir = None;
+								ns = None;
+								wires.push(v);
+								v = Vec::new();
+							},
+			44			=>	{
+								v.push(match dir {
+									Some('U')	=>	Direction::Up(ns.unwrap()),
+									Some('D')	=>	Direction::Down(ns.unwrap()),
+									Some('L')	=>	Direction::Left(ns.unwrap()),
+									Some('R')	=>	Direction::Right(ns.unwrap()),
+									_			=>	unreachable!(),
+								});
+								dir = None;
+								ns = None;
+							},
+			n @ 48..=57	=>	if let Some(x) = ns {
+								ns = Some(10*x + n - 48)
+							}
+							else {
+								ns = Some(n - 48)
+							},
+			_			=>	dir = Some(c),
 		};
 	}
 	wires
@@ -81,6 +108,10 @@ fn main () -> std::io::Result<()> {
 	let mut file = File::open("input03")?;
 	let mut data = String::new();
 	file.read_to_string(&mut data)?;
-	println!("{:?}",parse(&data));
+	let mut wg = WireGrid::new([1024,1024]);
+	for (i,ds) in parse(&data).iter().enumerate() {
+		let w = Wire::new(i);
+		wg.add(&ds,w);
+	}
 	Ok(())
 }
