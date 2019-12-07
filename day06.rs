07f9a6400128b75e86e304876a7191370f570ba0
Frans Saukko <frans@equilibrium.co>
2019-12-07 12:20:08 +0200

    Refactor aoc2019d06

--- day06.rs
+++ day06.rs
@@ -2,83 +2,98 @@ use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
 
+#[derive(Debug)]
+struct Orbit {
+	id: String,
+	parent: Option<usize>,
+}
+
+impl Orbit {
+	fn new (id: &str, parent: Option<usize>) -> Orbit {
+		Orbit {
+			id: id.to_owned(),
+			parent,
+		}
+	}
+}
+
 pub struct Orbits {
-	orbits: HashMap<String,Vec<String>>,
+	orbits: Vec<Orbit>,
 }
 
 impl Orbits {
-	pub fn new (hm: HashMap<String,Vec<String>>) -> Orbits {
+	pub fn new (v: Vec<(String,String)>) -> Orbits {
+		let mut hm = HashMap::new();
+		let mut os = Vec::new();
+		for ss in &v {
+			if !hm.contains_key(&ss.0) {
+				os.push(Orbit::new(&ss.0,None));
+				hm.insert(ss.0.to_owned(),os.len() - 1);
+			}
+			if !hm.contains_key(&ss.1) {
+				os.push(Orbit::new(&ss.1,Some(*hm.get(&ss.0).unwrap())));
+				hm.insert(ss.1.to_owned(),os.len() - 1);
+			}
+			else {
+				os[*hm.get(&ss.1).unwrap()].parent = Some(*hm.get(&ss.0).unwrap());
+			}
+		}
 		Orbits {
-			orbits: hm,
+			orbits: os,
 		}
 	}
 
-	fn suborbits (&self, k: &str) -> u32 {
-		if let Some(os) = self.orbits.get(k) {
-			let mut x = 0;
-			for o in os {
-				x += self.suborbits(o) + 1;
-			}
-			x
+	fn suborbits (&self, mut i: usize) -> u32 {
+		let mut n = 0;
+		while let Some(p) = self.orbits[i].parent {
+			n += 1;
+			i = p;
 		}
-		else {
-			0
-		}
+		n
 	}
 
 	pub fn orbits (&self) -> u32 {
-		let mut x = 0;
-		for o in &self.orbits {
-			x += self.suborbits(o.0);
+		let mut n = 0;
+		for i in 0..self.orbits.len() {
+			n += self.suborbits(i);
 		}
-		x
+		n
 	}
 
-	pub fn find<'a> (&'a self, mut k: &'a str) -> Vec<String> {
+	pub fn find (&self, k: &str) -> Vec<String> {
 		let mut v = Vec::new();
-		'outer: loop {
-			for os in &self.orbits {
-				for o in os.1 {
-					if o == k {
-						v.push(o.to_owned());
-						k = os.0;
-						continue 'outer;
-					}
+		for o in &self.orbits {
+			if o.id == k {
+				let mut p = o.parent;
+				while let Some(i) = p {
+					v.push(self.orbits[i].id.to_owned());
+					p = self.orbits[i].parent;
 				}
 			}
-			break;
 		}
 		v
 	}
 }
 
-fn parse (x: &str) -> HashMap<String,Vec<String>> {
-	let mut hm = HashMap::<String,Vec<String>>::new();
+fn parse (x: &str) -> Vec<(String,String)> {
+	let mut v = Vec::new();
 	let mut s = String::new();
-	let mut par = String::new();
+	let mut s0 = String::new();
 	for c in x.chars() {
 		match c as u8 {
 			10	=>	{
-						let mut v = hm.get_mut(&par);
-						if let Some(ref mut v) = v {
-							v.push(s.to_owned());
-						}
-						else {
-							let mut vs = Vec::new();
-							vs.push(s.to_owned());
-							hm.insert(par.to_owned(),vs);
-						}
+						v.push((s0.to_owned(),s.to_owned()));
 						s.clear();
 					},
 			13	=>	(),
 			41	=>	{
-						par = s.to_owned();
+						s0 = s.to_owned();
 						s.clear();
 					}
 			_	=>	s.push(c),
 		};
 	}
-	hm
+	v
 }
 
 fn main () -> std::io::Result<()> {
@@ -86,11 +101,12 @@ fn main () -> std::io::Result<()> {
 	let mut file = File::open("input06")?;
 	file.read_to_string(&mut data)?;
 	let orbits = Orbits::new(parse(&data));
+	println!("{}",orbits.orbits());
 	let (mut to_you,mut to_san) = (orbits.find("YOU"),orbits.find("SAN"));
 	while to_you.last() == to_san.last() {
 		to_you.pop();
 		to_san.pop();
 	}
-	println!("{}",to_you.len() + to_san.len() - 2);
+	println!("{}",to_you.len() + to_san.len());
 	Ok(())
 }
