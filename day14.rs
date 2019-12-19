41b5295b70333aad6479a34c538f7016f98f6fcb
Frans Saukko <frans@equilibrium.co>
2019-12-19 17:23:45 +0200

    aoc2019d14: implement reaction input

--- day14.rs
+++ day14.rs
@@ -1,39 +1,61 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 
-fn parse (s: &str) -> Vec<(i32,String)> {
-	let mut v = Vec::new();
+#[derive(Debug)]
+pub struct Chemical {
+	ingredients: Vec<(u32,String)>,
+	batch: u32,
+}
+
+impl Chemical {
+	pub fn new (v: &[(u32,String)], n: u32) -> Chemical {
+		Chemical {
+			ingredients: v.to_vec(),
+			batch: n,
+		}
+	}
+}
+
+pub struct Chemicalizer {
+	chemicals: HashMap<String,Chemical>,
+}
+
+fn parse (s: &str) -> HashMap<String,Chemical> {
+	let mut hm = HashMap::new();
+	let mut is = Vec::new();
 	let mut r = (0,String::new());
 	let mut n = String::new();
 	for c in s.chars() {
 		match c as u8 {
 			10			=>	{
-								v.push((r.0,r.1.to_owned()));
+								hm.insert(r.1.to_owned(),Chemical::new(&is,r.0));
 								r.0 = 0;
 								r.1 = String::new();
+								is.clear();
 							},
 			44			=>	{
-								v.push((r.0,r.1.to_owned()));
+								is.push((r.0,r.1.to_owned()));
 								r.0 = 0;
-								r.1 = String::new();
+								r.1.clear();
 							},
 			48..=57		=>	n.push(c),
 			61			=>	{
-								v.push((r.0,r.1.to_owned()));
+								is.push((r.0,r.1.to_owned()));
 								r.0 = 0;
-								r.1 = String::new();
+								r.1.clear();
 							},
 			65..=90		=>	{
 								if r.1.is_empty() {
 									r.0 = str::parse(&n).unwrap();
-									n = String::new();
+									n.clear();
 								}
 								r.1.push(c);
 							},
 			_			=>	(),
 		};
 	}
-	v
+	hm
 }
 
 fn main () -> std::io::Result<()> {
